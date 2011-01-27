8cfb3f0427629ed54c0cc1d532e78b10bba5ef51
Daniel Kennett <danielkennett@mac.com>
2011-01-27 22:07:35 +0100

    Cleaned up Xcode project. Added warning to point out that the client and secret keys need to be replaced for client apps.

--- Classes/Framework/ReadmillAPIConstants.h
+++ Classes/Framework/ReadmillAPIConstants.h
@@ -8,5 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
+#warning Replace these with your own application's Readmill client and secret keys.
+
 static NSString * const kReadmillClientSecret = @"750452e5fc0c20531e94f44215475094";
 static NSString * const kReadmillClientId = @"99dce8a929298cb95e534e86861db6de";
