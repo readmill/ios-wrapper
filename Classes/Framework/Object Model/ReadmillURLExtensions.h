87ac88d977af7a72a92a186f2ade7f35d2957c28
Martin Hwasser <martin.hwasser@gmail.com>
2011-04-10 00:52:28 +0200

    URL extensions supporting building of URLs.

---
+++ Classes/Framework/Object Model/ReadmillURLExtensions.h
@@ -0,0 +1,16 @@
+//
+//  ReadmillURLExtensions.h
+//  Readmill
+//
+//  Created by Martin Hwasser on 4/9/11.
+//  Copyright 2011 Readmill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ReadmillURLExtensions : NSObject {
+    
+}
+
+@end
---
+++ Classes/Framework/Object Model/ReadmillURLExtensions.m
@@ -0,0 +1,14 @@
+//
+//  ReadmillURLExtensions.m
+//  Readmill
+//
+//  Created by Martin Hwasser on 4/9/11.
+//  Copyright 2011 Readmill. All rights reserved.
+//
+
+#import "ReadmillURLExtensions.h"
+
+
+@implementation ReadmillURLExtensions
+
+@end
